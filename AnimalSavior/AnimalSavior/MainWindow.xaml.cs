67de42bab31cba4a81de51ecc0581aa19b429a1b
Abyanzhafran <52287739+Abyanzhafran@users.noreply.github.com>
2020-11-19 14:07:41 +0700

    Update lagi

--- AnimalSavior/AnimalSavior/MainWindow.xaml.cs
+++ AnimalSavior/AnimalSavior/MainWindow.xaml.cs
@@ -24,5 +24,10 @@ namespace AnimalSavior
         {
             InitializeComponent();
         }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
     }
 }
